9e6ca14f5cb3be1f64f3d72b27697ff77136c7c1
Avantika Gupta <avantikagupta1729@gmail.com>
2024-09-15 23:38:15 +0530

    Seventh Commit

--- Arrays/WavePrint.cpp
+++ Arrays/WavePrint.cpp
@@ -1,30 +1,27 @@
-// Each Element occurs twice except for one
 #include <bits/stdc++.h>
 using namespace std;
-void WavePrint(int m,int n,vector<vector<int>>&arr){
-    for(int j=0;j<n;j++){
-        if(j%2==0){
-            for(int i=0;i<m;i++)
-                cout<<arr[i][j];
-        }
-        else{
-            for(int i=m-1;i>=0;i--)
-                cout<<arr[i][j];
-        }
-    }
+#define R 5
+#define C 4
+void WavePrint(int m, int n, int arr[R][C])
+{
+	for (int j = 0; j < n; j++) {
+		if (j % 2 == 0) {
+			for (int i = 0; i < m; i++) 
+				cout << arr[i][j] << " ";
+		}
+		else {
+			for (int i = m - 1; i >= 0; i--) 
+				cout << arr[i][j] << " ";
+		}
+	}
 }
-int main(){
-    int n,m,ele;
-    vector<vector<int>> arr1;
-    cout<<"Enter the number of rows and columns int the array : ";
-    cin>>m>>n;
-    cout<<"Enter the elements of the array: ";
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-        cin>>ele;
-        arr1[i][j]=ele;
-    }
-    }
-    WavePrint(m,n,arr1);
-   return 0;
+int main()
+{
+	int arr[R][C] = { { 1, 2, 3, 4 },
+					{ 5, 6, 7, 8 },
+					{ 9, 10, 11, 12 },
+					{ 13, 14, 15, 16 },
+					{ 17, 18, 19, 20 } };
+	WavePrint(R, C, arr);
+	return 0;
 }
